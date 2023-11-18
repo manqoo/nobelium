58853692f76411b8dfc1d9a619b83ea4ba723b4c
manqoo <60530548+manqoo@users.noreply.github.com>
2023-11-18 09:38:43 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -8,11 +8,11 @@ const BLOG = {
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'light', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
+  lightBackground: '#FFDE5B', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  since: 2023, // If leave this empty, current year will be used.
+  postsPerPage: 3,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
