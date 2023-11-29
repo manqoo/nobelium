1e24644cfc2654d6c7ac7f4c6656662ce41ed805
manqoo <60530548+manqoo@users.noreply.github.com>
2023-11-29 15:34:40 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ const NavBar = () => {
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.ALBUM, to: '/album', show: true },
+    { id: 2, name: locale.NAV.RSS, to: '/subscribe', show: true },
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
     
   ]
