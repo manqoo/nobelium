185039725e295b4da31db6128173766102e05ef0
manqoo <60530548+manqoo@users.noreply.github.com>
2024-02-01 16:16:55 +0800

    Update BlogPost.js

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -17,7 +17,7 @@ const BlogPost = ({ post }) => {
           </time>
         </header>
         <main>
-          <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
+          <p className="hidden md:block leading-8 text-green-600 dark:text-green-600">
             {post.summary}
           </p>
         </main>
