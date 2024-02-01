81e77907ac5ff3b7803c28ac52fe3a0ee1a73e7e
manqoo <60530548+manqoo@users.noreply.github.com>
2024-02-01 15:46:16 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -12,7 +12,7 @@ const BLOG = {
   darkBackground: '#F2FFF0', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2023, // If leave this empty, current year will be used.
-  postsPerPage: 3,
+  postsPerPage: 5,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
